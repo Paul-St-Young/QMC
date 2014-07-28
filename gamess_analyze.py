71ef787ea0fde0662380860908adbae2c5b94bce
Paul Young <yyang173@illinois.edu>
2014-07-28 10:33:22 -0500

    v0.47 Minor improvement to gamess_analyze.py

--- gamess_analyze.py
+++ gamess_analyze.py
@@ -2,8 +2,10 @@
 """
 GAMESS output analysis script
 Author: Paul Young
-Last Modified: July 9 2014
+Last Modified: July 28 2014
 """
+
+ENTRIES=["TOTAL ENERGY =", "FINAL MCSCF ENERGY IS"]
 
 import sys
 
@@ -11,11 +13,11 @@ class GAMESS:
 	def __init__(self,gmsout):
 		self.gmsout = gmsout
 			
-	def grab(self,entry):
+	def grab(self,entry,line):
 	# prints out the first entry it finds in the output
-		for line in open(self.gmsout,'r'):
-			if line.find(entry)!=-1:
-				print entry,line.split()[-1]
+		if line.find(entry)!=-1:
+			#print entry,line.split()[-1]
+			print line.lstrip()[:-1]
 				
 	def count_basis(self):
 	# count # basis determinants, 
@@ -31,10 +33,16 @@ class GAMESS:
 			if in_basis and n_basis<MAX_N_BASIS and line.find("|")!=-1:
 				n_basis+=1
 		return n_basis/2-2
+		
+	def main_loop(self):
+		for line in open(self.gmsout,'r'):
+			for entry in ENTRIES:
+				self.grab(entry,line)
+			
 
 
 beh=GAMESS(sys.argv[1])
 print "============================================="
 print " Analyzing GAMESS output " + sys.argv[1]
-beh.grab("TOTAL ENERGY =")
+beh.main_loop()
 print "TOTAL NUMBER OF DET =",beh.count_basis()
