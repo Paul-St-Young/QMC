d50e6069f5b72d6fba32ed9430c7455f90e527a0
Paul Young <yyang173@illinois.edu>
2015-03-17 12:53:08 -0500

    v0.70 added mix_fix

--- gofr.py
+++ gofr.py
@@ -3,21 +3,31 @@
 import h5py
 import pandas
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import numpy as np
 import argparse
 
-slices = [0,-1]
-species_map={0:'C',1:'N',2:'H'}
+slices = [0,1,2,3]
+species_map={'0':'C','1':'N','2':'H'}
+#species_map={'0':'Li','1':'H'}
 
 def timeSlice(gofr, windowSize):
     nbins = int(gofr.get("cutoff")[0]/gofr.get("delta")[0]) # number of bins
+    
+    # get bin centers
     r=np.arange( nbins ) * gofr.get("delta")[0]
+    for i in range(len(r)):
+        r[i] += dr/2.0
+    # end i
+    
+    # get all time slices of g(r)
     GR=[]
     for nwindow in range( 0,len(gofr.get("value"))/windowSize ):
     
        GR.append( gofr.get("value")[nwindow*windowSize:(nwindow+1)*windowSize].mean(axis=0) )
 
     # end for nwindow
+    
     return r,GR
 # end def timeSlice
 
@@ -27,6 +37,38 @@ def normalize(r,dr,gr):
     # end for
 # end def normalize
 
+def average(r,gr):
+    return sum(r*gr)/sum(gr)
+# end def average
+
+def gaussian(x, *p):
+    A, mu, sigma = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+# end def gaussian
+
+def fit_gaussian(r,gr,start,end):
+    p0 = [1., average(r,ar[k]) , 1.]
+    coeff, var_matrix = curve_fit(gaussian, r[start:end], gr[start:end], p0=p0)
+    return coeff
+# end def fit_gaussian
+
+def get_start_and_end(gr,thres):
+    started = False
+    start = 0
+    end = len(gr)-1
+    for i in range(len(gr)):
+        if (not started) and gr[i]>thres:
+            started = True
+            start = i
+        # end if not started
+        if started and gr[i]<thres:
+            end = i
+            break
+        #end if started
+    # end for
+    return start,end
+# end def
+
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Plot time evolution of g(r)')
@@ -34,23 +76,19 @@ if __name__=="__main__":
     parser.add_argument("-w", "--windowSize", type=int, default=50, help="size of time slice window in steps" )
     args = parser.parse_args()
 
+    start = 0 
+    end = 0
+
     # get name of molecule
     mol = args.h5file.split(".")[0] # name of molecule
-    #species_map={}
-    #for i in range(len(mol.strip("+").strip("-"))):
-        #atom = mol[i]
-        #species_map[i]=atom
-    # end i
 
     f = h5py.File(args.h5file)
     for name,quantity in f.items():
         if name.startswith('gofr'):
             g,p,i,j = name.split('_')
-            i = int(i)
-            j = int(j)
             dr = quantity.get("delta")[0]
-            
-            if (i!=j):
+            if (i!=j and i=='0' and j=='2'):
+            #if (i!=j):
                 windowSize = args.windowSize
                 nwindow = len(quantity.get("value"))/windowSize
                 r,GR = timeSlice(f.get(name), windowSize)
@@ -64,20 +102,30 @@ if __name__=="__main__":
                 
                 label = slices[:]
                 for k in range(len(slices)):
+                
                     label[k] = "window "+str(slices[k]+1)
-                    print species_map[i]+"-"+species_map[j] + " window " + str(slices[k]+1) + " " + str( (ar[k]*r).mean()/ar[k].mean() )
+                    if slices[k]==-1:
+                        slices[k]=nwindow-1
+                    # end if
+                    start,end = get_start_and_end(ar[k],1e-4)
+                    coeff = fit_gaussian(r,ar[k],start,end)
+                    A,mean,sigma = coeff
+                    print species_map[i]+"-"+species_map[j] + " window " + str(slices[k]+1) + " mu=" + str( round(mean,3) ) + " var=" + str( round(sigma*sigma,3) )
+                    #print sum(ar[k]*r)/sum(ar[k])
+                    
                 # end for k
                 
                 df = pandas.DataFrame(ar.T,index=r,columns=label)
                 ax = df.plot(title=mol+" run split into "+str(nwindow)+" windows each of size "+str(windowSize))
                 ax.set_xlabel(species_map[i]+"-"+species_map[j]+" (a.u.)")
-                ax.set_ylabel("g(r)")
+                ax.set_ylabel("P(r)")
+
+                #plt.plot(r[start:end], gaussian(r[start:end],*coeff), label='Fitted data')
+
             # end if i!=j
+            
         # end if name.startswith
     # end for name,quantity
     
-    
-    plt.show()
-    
-    
+    plt.show() 
 # end __main__
---
+++ mix_fix.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+import argparse
+
+from scipy.optimize import curve_fit
+
+def gofrGrabber(h5file,myi,myj):
+    # get gofr list with label "gofr_ion0_myi_myj"
+    dr = 0.0
+    r = []
+    GR = []
+    
+    f = h5py.File(h5file)
+    for name,quantity in f.items():
+        if name.startswith('gofr'):
+            g,p,i,j = name.split('_')
+            if i==str(myi) and j==str(myj):
+                dr = quantity.get("delta")[0]
+                maxr = quantity.get("cutoff")[0]
+                r = np.arange( int(maxr/dr) ) * dr
+                for k in range(len(r)):
+                    r[k] += dr/2.0
+                # end for k
+                GR = quantity.get("value")[:]
+        # end if name.startswith
+    # end for name,quantity
+    f.close()
+    
+    return r,dr,GR
+# end def gofrGrabber
+
+def gaussian(x, *p):
+    A, mu, sigma = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+# end def gaussian
+
+def fit_gaussian(r,gr,start,end):
+    p0 = [1., average(r,ar[k]) , 1.]
+    coeff, var_matrix = curve_fit(gaussian, r[start:end], gr[start:end], p0=p0)
+    return coeff
+# end def fit_gaussian
+
+def list_to_list(block):
+    block = block.strip("[").strip("]").strip("\n")
+    return block.split(",")
+# end def 
+
+def norm(dr,Pr):
+    A = 0.0
+    for p in Pr:
+        A += p*dr
+    # end for p
+    return A
+# end def norm
+
+def normalize(r,dr,gr):
+    A = 0.0
+    for i in range(len(gr)):
+        A += gr[i]*dr
+    # end for
+    for i in range(len(gr)):
+        gr[i] /= A
+    # end for
+# end def 
+
+def get_start_and_end(gr,thres):
+    started = False
+    start = 0
+    end = len(gr)-1
+    for i in range(len(gr)):
+        if (not started) and gr[i]>thres:
+            started = True
+            start = i
+        # end if not started
+        if started and gr[i]<thres:
+            end = i
+            break
+        #end if started
+    # end for
+    return start,end
+# end def    
+
+def average(r,gr):
+    return sum(r*gr)/sum(gr)
+# end def average
+
+def gaussian(x, *p):
+    A, mu, sigma = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+# end def gaussian
+
+def fit_gaussian(r,gr,start,end):
+    p0 = [1., average(r,gr) , 1.]
+    coeff, var_matrix = curve_fit(gaussian, r[start:end], gr[start:end], p0=p0)
+    return coeff
+# end def fit_gaussian
+
+def mix(vgr,dgr,eps):
+    vstart, vend = get_start_and_end(vgr,eps)
+    dstart, dend = get_start_and_end(dgr,eps)
+    start = max(vstart,dstart)
+    end = min(vend,dend)
+    
+    lnfix = list( np.zeros( min(vstart,start) ) )
+    if start > vstart:
+        lnfix += list( np.zeros(start-vstart) )
+    # end if
+    
+    for i in range(start,end):
+        lnfix.append(dgr[i]/vgr[i])
+    # end i
+    
+    lnfix += list( np.zeros(len(vgr)-end) )
+    
+    return lnfix  
+# end def mix
+
+def quick_fix(vgr,dgr,eps):
+    fixed_gr = []
+    for i in range(len(vgr)):
+        if vgr[i]>eps:
+            fixed_gr.append(dgr[i]*dgr[i]/vgr[i])
+        else:
+            fixed_gr.append(dgr[i])
+        # end if
+    # end for
+    return fixed_gr
+# end def quick_fix
+
+def cut(r,f,r1,r2): # cut out a piece of f and pad with 0
+    cutgr = []
+    for i in range(len(r)):
+        if r[i]>r1 and r[i]<r2:
+            cutgr.append(f[i])
+        else:
+            cutgr.append(0)
+        # end if
+    # end for
+    return cutgr
+# end def cut
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Use g(r) to fix wf')
+    parser.add_argument("-vw", "--VMCwindowSize", type=int, default=500
+                        , help="size of time slice window in steps" )
+    parser.add_argument("-dw", "--DMCwindowSize", type=int, default=500
+                        , help="size of time slice window in steps" )
+    args = parser.parse_args()
+
+    r,dr,VGR = gofrGrabber("HCN+.s001.stat.h5",'0','2')
+    vgr = VGR[len(VGR)-args.VMCwindowSize:].mean(axis=0)
+    normalize(r,dr,vgr)
+    r,dr,DGR = gofrGrabber("HCN+.s002.stat.h5",'0','2')
+    dgr = DGR[len(DGR)-args.DMCwindowSize:].mean(axis=0)
+    normalize(r,dr,dgr)
+    
+    mean_vgr = []
+    for i in range(0,len(vgr)-5,6):
+        mean_vgr.append( sum(vgr[i:i+6])/6. )
+    # end for i
+    vgr = mean_vgr[:]
+
+    # both fixed
+    fixed_gr = quick_fix(vgr,dgr,1e-3)
+    start,end = get_start_and_end(vgr,1e-3)
+    coeff = fit_gaussian(r,fixed_gr,start,end)
+    print coeff[1]
+    normalize(r,dr,fixed_gr)
+    print sum(fixed_gr*r)/sum(fixed_gr)
+   
+
+    ar = np.array( fixed_gr )
+    df = pandas.DataFrame(ar.T,index=r)
+    df.plot(title=r"Probability Distribution of $r_{CH}$")
+    plt.plot(r[start:end], gaussian(r[start:end],*coeff), label='Fitted data')
+    
+    plt.show()
+# end __main__
