0564d8ef4853668579d92df32558c244a510eda1
Paul Young <yyang173@illinois.edu>
2016-05-31 19:14:58 -0500

    add extract_scalar() to nexus_addon

--- nexus_addon.py
+++ nexus_addon.py
@@ -27,3 +27,43 @@ class MyTextFile(TextFile):
         return trim_head_tail.strip("\n")
     # end def 
 # end class
+
+def extract_scalar( qmc_run
+    ,extract_names   = ["LocalEnergy","LocalEnergyVariance"]
+    ,extract_attribs = ["mean","error"] ):
+
+    """ Given a qmc run, extract desired scalar quantities. Return a dictionary of the extracted results. Available attributes can be found by printing qmc_run.scalars.keys(). """
+
+    entry = {}
+
+    for name in qmc_run.scalars.keys():
+        if name in extract_names:
+            scalar = qmc_run.scalars[name]
+            for attrib in scalar.keys():
+                if attrib in extract_attribs:
+                    entry[name+"_"+attrib] = scalar[attrib]
+                # end if
+            # end for
+        # end if
+    # end for
+    
+    return entry
+# end def
+
+def scalar_rename(name):
+    my_names = {
+        "LocalEnergyVariance_error":"Ve",
+        "LocalEnergyVariance_mean":"V",
+        "LocalEnergy_mean":"E",
+        "LocalEnergy_error":"Ee"
+    }
+    
+    
+    if name in my_names:
+        new_name = my_names[name] 
+    else:
+        new_name = name
+    # end if
+    
+    return new_name
+# end def
