1fbe686774d823bc5ed3ae31d6fdc0bba79da88d
Paul Young <yyang173@illinois.edu>
2016-12-23 14:10:12 -0600

    better backup script
    known bug: fails for missing sym. link

--- backup_scripts.py
+++ backup_scripts.py
@@ -3,11 +3,11 @@ import os
 import sys
 import subprocess as sp
 
-def backup_target_list(target_dir,name_fmts):
+def backup_target_list(source_dir,name_fmts):
     ''' generate a list of files to backup '''
     targets = []
     for name_fmt in name_fmts:
-        proc = sp.Popen('find %s -name "%s" | grep -v %s' % (target_dir,name_fmt,sys.argv[0])
+        proc = sp.Popen('find %s -name "%s" | grep -v %s' % (source_dir,name_fmt,sys.argv[0])
             ,shell=True,stdout=sp.PIPE,stderr=sp.PIPE)
         out,err = proc.communicate()
 
@@ -20,26 +20,28 @@ def backup_in_dir(backup_dir,targets):
 
     for target in targets:
         subdir = os.path.dirname(target)
-        target_dir = backup_dir+'/'+subdir
+        target_dir = os.path.join(backup_dir,subdir)
         if not os.path.exists(target_dir):
-            sp.call(['mkdir','-p',target_dir])
+            failed = sp.call(['mkdir','-p',target_dir])
+            if failed:
+                raise RuntimeError( "failed to create directory %s" % target_dir )
+            # end if
         # end if
         failed = sp.call(['cp',target,target_dir])
         if failed:
-            raise RunTimeError( "cp failed for %s to %s" % (target,target_dir) )
+            raise RuntimeError( "cp failed for %s to %s" % (target,target_dir) )
         # end if
     # end for
     
 # end def
 
 if __name__ == '__main__':
-    target_dir = '.'
-    source_name= sys.argv[1]
+    source_dir = sys.argv[1]
     name_fmts  = ['*.py','*.ipnb']
 
-    targets = backup_target_list(target_dir,name_fmts)
+    targets = backup_target_list(source_dir,name_fmts)
 
-    backup_dir = 'scripts_%s' % source_name
+    backup_dir = 'scripts_%s' % source_dir
 
     backup_in_dir(backup_dir,targets)
 # end __main__
