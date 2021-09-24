62ec2e503f8814c0e1a9abdf516e617c93916a3e
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2021-09-24 14:31:24 -0400

    move argparse to main function

--- boil/pymain.py
+++ boil/pymain.py
@@ -5,11 +5,12 @@ import numpy as np
 #import pandas as pd
 #import matplotlib.pyplot as plt
 
-def main(args):
-  pass
-
-if __name__ == '__main__':
+def main():
   from argparse import ArgumentParser
   parser = ArgumentParser()
+  #parser.add_argument('input_filename', type=str)
+  parser.add_argument('--verbose', action='store_true')
   args = parser.parse_args()
-  main(args)
+
+if __name__ == '__main__':
+  main()  # set no global variable
